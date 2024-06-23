115e3bd24577707a847b58f2260298ee2b5253c9
IamFastre <mahmoudmode057@gmail.com>
2024-06-23 07:55:00 +0300

    different layout & colors + easter egg

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,25 @@ import consts from "@/utils/consts";
 import Image from "next/image";
 
 export default function Home() {
+  const imgSize:number = 200;
+
   return (
     <>
-      <Section title="Winery DeFastre" style={{ flex: 1 }}>
+      <Section title="Winery DeFastre" style={{ flex: 1 }} containerStyle={{ justifyContent: 'center' }} centered>
         <div style={{ alignItems: 'center', gap: "15px" }}>
           <Image
             src={`${consts.basePath}/static/images/NaipeDeCopas.png`}
             alt="the Winery logo; a purple heart suit with a sharp spiky hexagram on top"
-            width={100}
-            height={100}
+            width={imgSize}
+            height={imgSize}
             priority
           />
           <span>
-            Hello world!
+            Hello, there!
           </span>
+          <a className="color-primary" href="https://youtu.be/rEq1Z0bjdwc" target="_blank">
+            General Kenobi!
+          </a>
         </div>
       </Section>
     </>
