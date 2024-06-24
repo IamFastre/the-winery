baf4a71c59253643c44005ccff7a6d3e1c955103
IamFastre <mahmoudmode057@gmail.com>
2024-06-24 16:43:56 +0300

    aww a beating heart!

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,73 @@
+"use client";
 import { Header, Section } from "@/components";
+import consts from "@/utils/consts";
 import Image from "next/image";
+import styles from "/styles/page.module.scss";
+import { useEffect, useState } from "react";
+
+const Nail = () => (
+  <div style={{ fontSize: 26 }}>
+    •
+  </div>
+);
 
 export default function Home() {
   const imgSize:number = 200;
+  const [showMsg, setShowMsg] = useState<boolean>(false);
+  const [clicks, setClicks] = useState<number>(0);
+  const [hrtCls, setHrtCls] = useState<string>(styles.icon);
+
+  const isBeating = () => hrtCls !== styles.icon;
+
+  useEffect(() => {
+    if (clicks) {
+      setTimeout(() => {
+        setClicks(0);
+      }, 1500);
+    }
+
+    if (clicks >= 3) {
+      setShowMsg(true);
+      setClicks(0);
+      setHrtCls(isBeating() ? styles.icon : `${hrtCls} ${styles.beating}`);
+      setTimeout(() => {
+        setShowMsg(false);
+      }, 3000);
+    }
+    }, [clicks])
 
   return (
     <div style={{ flex: 1, margin: "10px" }}>
       <Header
         title="The Winery"
-        left={(
-            <div>•</div>
-        )}
-        right={(
-            <div>•</div>
-        )}
-        margin="20px"
+        left={<Nail />}
+        right={<Nail />}
+        margin="17px"
       />
 
       <Section style={{ flex: 1 }} centered>
         <div style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: "15px" }}>
-          <Image
-            src={'/static/images/NaipeDeCopas.png'}
-            alt="the Winery logo; a purple heart suit with a sharp spiky hexagram on top"
-            width={imgSize}
-            height={imgSize}
-            priority
-          />
+          <div
+            onClick={() => setClicks(c => c + 1)}
+            className={hrtCls}
+          >
+            <Image
+              src={'/static/images/NaipeDeCopas.png'}
+              alt="the Winery logo; a purple heart suit with a sharp spiky hexagram on top"
+              width={imgSize}
+              height={imgSize}
+              priority
+            />
+          </div>
           <span>
             Hello, there!
           </span>
           <a className="color-primary" href="https://youtu.be/rEq1Z0bjdwc" target="_blank">
             General Kenobi!
           </a>
+          <span style={{ opacity: showMsg ? 1 : 0 }} className={styles.msg}>
+            Wish {isBeating() ? "CPR" : "giving someone a cardiac arrest"} was that easy...
+          </span>
         </div>
       </Section>
     </div>
