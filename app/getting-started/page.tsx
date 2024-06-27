b14c60cf72fcfe03f69752a4fad36d93b2d4c3c8
IamFastre <mahmoudmode057@gmail.com>
2024-06-27 05:20:23 +0300

    removed that useless layout

--- app/getting-started/page.tsx
+++ app/getting-started/page.tsx
@@ -3,11 +3,17 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { IoWine } from "react-icons/io5";
 
-import { B, Button, C, Section } from "@/components";
+import { B, Button, C, Header, Section } from "@/components";
 
 import NDC from "@/public/static/images/NaipeDeCopas.png";
 import styles from "./getting-started.module.scss";
 
+
+const Nail = () => (
+  <div style={{ fontSize: 26 }}>
+    •
+  </div>
+);
 
 export default function GettingStarted() {
   const [showMsg, setShowMsg] = useState<boolean>(false);
@@ -34,51 +40,59 @@ export default function GettingStarted() {
     }, [clicks])
 
   return (
-    <Section style={{ flex: 1 }} centered>
-      <div className={styles.holder}>
-        <div
-          onClick={() => setClicks(c => c + 1)}
-          className={hrtCls}
-        >
-          <Image
-            src={NDC}
-            alt="the Winery logo; a purple heart suit with a sharp spiky hexagram on top"
-            className={styles.image}
-            priority
+    <div style={{ flex: 1, margin: 10 }}>
+      <Header
+        title="The Winery"
+        left={<Nail />}
+        right={<Nail />}
+        margin="20px"
+      />
+      <Section style={{ flex: 1 }} centered>
+        <div className={styles.holder}>
+          <div
+            onClick={() => setClicks(c => c + 1)}
+            className={hrtCls}
+          >
+            <Image
+              src={NDC}
+              alt="the Winery logo; a purple heart suit with a sharp spiky hexagram on top"
+              className={styles.image}
+              priority
+            />
+          </div>
+          <span className={styles.text}>
+            Hello, there!
+            <br/>
+            <a href="https://youtu.be/rEq1Z0bjdwc" target="_blank" style={{ textDecoration: 'none' }}>
+              <C.NONE>
+                  General Kenobi!
+              </C.NONE>
+            </a>
+          </span>
+
+          <div className={styles.sep} />
+
+          <span className={styles.desc}>
+            Welcome to <C.ACCENT><B>The Winery</B></C.ACCENT> where, no sadly, we don't make wine, but where we whine!
+            <br/>
+            To get start and ferment your own pieces, please <a href="">log in</a> to your account.
+            <br/>
+            Oh you're new? expected; <a href="">create new account</a>!
+          </span>
+
+          <Button
+            title="Get started"
+            icon={{
+              element: IoWine,
+            }}
+            onClick={() => alert("Still working on that :)")}
           />
+
+          <span style={{ opacity: showMsg ? 1 : 0 }} className={styles.msg}>
+            Wish {isBeating() ? "CPR" : "giving someone a cardiac arrest"} was that easy...
+          </span>
         </div>
-        <span className={styles.text}>
-          Hello, there!
-          <br/>
-          <a href="https://youtu.be/rEq1Z0bjdwc" target="_blank" style={{ textDecoration: 'none' }}>
-            <C.NONE>
-                General Kenobi!
-            </C.NONE>
-          </a>
-        </span>
-
-        <div className={styles.sep} />
-
-        <span className={styles.desc}>
-          Welcome to <C.ACCENT><B>The Winery</B></C.ACCENT> where, no sadly, we don't make wine, but where we whine!
-          <br/>
-          To get start and ferment your own pieces, please <a href="">log in</a> to your account.
-          <br/>
-          Oh you're new? expected; <a href="">create new account</a>!
-        </span>
-
-        <Button
-          title="Get started"
-          icon={{
-            element: IoWine,
-          }}
-          onClick={() => alert("Still working on that :)")}
-        />
-
-        <span style={{ opacity: showMsg ? 1 : 0 }} className={styles.msg}>
-          Wish {isBeating() ? "CPR" : "giving someone a cardiac arrest"} was that easy...
-        </span>
-      </div>
-    </Section>
+      </Section>
+    </div>
   );
 }
