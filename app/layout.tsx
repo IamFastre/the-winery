edf8bf385686ce9c77bd0d010237066ceb657b85
IamFastre <mahmoudmode057@gmail.com>
2024-07-03 14:20:41 +0300

    metadata shit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import consts from "@/utils/consts";
 import "@/styles/globals.scss";
 
 
-export const metadata: Metadata = {
+export const metadata:Metadata = {
   title: consts.name,
   description: "Welcome to The Winery where, no sadly, we don't make wine, but where we whine!",
 };
+
+export const viewport:Viewport = {
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  width: 'device-width',
+}
 
 export default function RootLayout({ children }: Readonly<{ children:React.ReactNode }>) {
   return (
