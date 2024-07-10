257de93b47e9e0da47c18898d574973858a57a6a
IamFastre <mahmoudmode057@gmail.com>
2024-07-10 07:21:44 +0300

    getting Vercel-y

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const __dirname  = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   distDir: 'dist',
   images: { unoptimized: true },
   sassOptions: {
