c5d26f85591eaf5f0f21b0b3de0043c16c794dcf
IamFastre <mahmoudmode057@gmail.com>
2024-07-13 16:29:51 +0300

    can now give redirect type

--- hooks/useGoTo.ts
+++ hooks/useGoTo.ts
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
-export function useGoTo() : [boolean, (path:string) => void, string] {
+export function useGoTo() : [boolean, (path:string, type?:'push' | 'replace') => void, string] {
   const router  = useRouter();
   const current = usePathname();
   const [redirecting, setRedirecting] = useState(false);
 
-  const goto = (path:string) => {
+  const goto = (path:string, type:'push' | 'replace' = 'push') => {
     setRedirecting(true);
 
     setTimeout(() => {
-      router.push(path);
+      if (type === 'push')
+        router.push(path);
+      else if (type === 'replace')
+        router.replace(path)
+      else
+        throw "wtf is that";
+
       setRedirecting(false);
-    }, 960)
+    }, 960);
   };
 
   return [redirecting, goto, current];
