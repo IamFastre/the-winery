e580da932a52ac835b5278a9cf1826655b72939b
IamFastre <mahmoudmode057@gmail.com>
2024-07-16 15:41:51 +0300

    now can pass empty string

--- utils/props.ts
+++ utils/props.ts
@@ -2,13 +2,14 @@ import { KeyboardEventHandler, MouseEventHandler } from "react";
 
 export function focusable(activeClass:string, onClick?:MouseEventHandler<HTMLElement>) {
   const onKeyDown:KeyboardEventHandler<HTMLElement> = e => {
-    if (e.key === 'Enter')
+    if (e.key === 'Enter' && activeClass)
       e.currentTarget.classList.add(activeClass);
   };
 
   const onKeyUp:KeyboardEventHandler<HTMLElement> = e => {
     if (e.key === 'Enter') {
-      e.currentTarget.classList.remove(activeClass);
+      if (activeClass)
+        e.currentTarget.classList.remove(activeClass);
 
       if (e.currentTarget.click)
         e.currentTarget.click();
