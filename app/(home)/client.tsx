775405b9bbd75a2acfd6a88bdb28f579fe007f1b
IamFastre <mahmoudmode057@gmail.com>
2024-07-21 23:01:32 +0300

    cards now update search parameters so you can reload safely

--- app/(home)/client.tsx
+++ app/(home)/client.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
 import { IoCogOutline, IoInformationCircleOutline, IoPersonCircleOutline, IoSearchOutline, IoWineOutline, IoArrowBack, IoArrowForward } from "react-icons/io5";
 
@@ -17,8 +18,34 @@ interface Props {
   users: { [identifier:string]:PublicProfile; };
 }
 
+const getCardIndex = (param:string | null, max:number | undefined) => {
+  if (!param)
+    return 1;
+
+  if (!max)
+    max = 1;
+
+  let result = Number.parseInt(param);
+
+  if (Number.isNaN(result) || result < 1)
+    return 1;
+
+  if (result > max)
+    return max;
+
+  return result;
+}
+
+const setCardIndex = (value:number) => {
+  const params = new URLSearchParams();
+  params.set('card', value.toFixed(0));
+  return params.toString();
+};
+
 export function FeedNavigator({ feed, users }:Props) {
-  const [index, setIndex] = useState<number>(0);
+  const card = getCardIndex(useSearchParams().get('card'), feed.length) - 1;
+ 
+  const [index, setIndex] = useState<number>(card);
   const post = feed[index];
   const author = users[post.author ?? ""];
 
@@ -27,6 +54,10 @@ export function FeedNavigator({ feed, users }:Props) {
       if (index + num < feed.length && index + num >= 0)
         setIndex(index + num);
   };
+
+  useEffect(() => {
+    window.history.pushState(null, '', `?${setCardIndex(index + 1)}`);
+  }, [index])
 
   return (
     <>
