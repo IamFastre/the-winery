f93457761d75eaf9a7c80410099fa5c6bec3be6a
IamFastre <mahmoudmode057@gmail.com>
2024-07-23 02:19:04 +0300

    compose page can now edit/delete/post drafts

--- app/(home)/compose/client.tsx
+++ app/(home)/compose/client.tsx
@@ -1,19 +1,117 @@
 "use client";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useRouter } from "next/navigation";
 import { IoEyeOutline, IoEye, IoFolderOutline, IoAdd, IoWine, IoFolder } from "react-icons/io5";
 import XRegExp from "xregexp";
 
-import { createPost, createDraft } from "@/supabase/actions/post";
+import { createPost, createDraft, editDraft, deleteDraft } from "@/supabase/actions/post";
 import { type AuthError, PostgrestError } from "@supabase/supabase-js";
-import { Database } from "@/supabase/types";
+import { Draft, Profile } from "@/supabase/actions/types";
 import { Section, MarkDown, Button, C } from "@/components";
 import { useToaster } from "@/providers/Toaster";
 
 import colors from '@/styles/colors.module.scss';
 import styles from "./styles.module.scss";
+import { RiBallPenLine, RiDeleteBin6Line } from "react-icons/ri";
 
-export function PostEditor({ user, error: userError }:{ user:Database['public']['Tables']['profiles']['Row']; error:AuthError | PostgrestError | null; }) {
+type State<T> = Dispatch<SetStateAction<T>>;
+
+const Editor = (props:{ title:string; content:string; show:boolean; setShow:State<boolean>; setTitle:State<string>; setContent:State<string>; setError: State<null>; }) => (
+  <Section
+    title={props.show ?
+      props.title :
+      <input
+        type="text"
+        name="span"
+        placeholder="Change title"
+        value={props.title}
+        onChange={e => props.setTitle(e.target.value)}
+        onBlur={() => props.setTitle(t => t.trim())}
+        style={{ width: (props.title ? props.title.length : 12) * 11.72 }}
+        autoComplete="off"
+        />
+      }
+    className={styles.card}
+    containerClassName={styles.cardContent}
+    isCard
+  >
+    {
+      props.show ?
+      <MarkDown>
+      {/* TODO: replace the link with an internal `/help/markdown` one */}
+      { props.content ? props.content : "### *Input Preview*\n*Try actually doing some [basic markdown](https://www.markdownguide.org/cheat-sheet/#basic-syntax), and some [extended syntax](https://www.markdownguide.org/cheat-sheet/#basic-syntax).*"}
+    </MarkDown>
+      :
+      <textarea
+        name="span"
+        placeholder="What's on your mind? (min. 16)"
+        value={props.content}
+        onChange={e => { props.setContent(e.target.value); props.setError(null); }}
+        onBlur={() => props.setContent(c => c.trim())}
+        autoComplete="off"
+      />
+    }
+    <Button
+      noMinimum
+      iconBackground
+      className={styles.showPrev}
+      onClick={() => props.setShow(!props.show)}
+      color={colors.highlight}
+      icon={{ element: props.show ? IoEyeOutline : IoEye }}
+    />
+  </Section>
+);
+
+const AsUser = ({ username }:{ username:string; }) => (
+  <div className={styles.asUser}>
+    <span>
+      <C.SECONDARY>
+        Posting as
+      </C.SECONDARY>
+      <C.QUINARY>
+        {' u:'}
+      </C.QUINARY>
+      <C.ACCENT>
+        {username}
+      </C.ACCENT>
+    </span>
+  </div>
+);
+
+const Error = (props:{ error:AuthError | PostgrestError; isOK:boolean; }) => (
+  <span className={styles.error}>
+    Oops...
+    <br/>
+    {
+      !props.isOK ?
+      <>
+        <span>
+          That's a really short thought, don't you think?
+        </span>
+      </>
+      :
+      <>
+        <span>
+          Seems an error has occurred, try again?
+        </span>
+        <br/>
+        <span>
+          if that didn't work <a href="https://github.com/IamFastre/the-winery/issues">fill a Github issue</a>.
+          {
+            Object.keys(props.error).length ?
+            <>
+              <br/>
+              {JSON.stringify(props.error)}
+            </>
+            : null
+          }
+        </span>
+      </>
+    }
+  </span>
+);
+
+export function PostEditor({ user }:{ user:Profile; }) {
   const router = useRouter();
   const toaster = useToaster();
 
@@ -21,20 +119,6 @@ export function PostEditor({ user, error: userError }:{ user:Database['public'][
   const [content, setContent] = useState<string>("");
   const [showPrev, setShowPrev] = useState<boolean>(false);
   const [error, setError] = useState<AuthError | PostgrestError | null>();
-
-  const placeholder = "Change title";
-  const input = (
-    <input
-      type="text"
-      name="span"
-      placeholder={placeholder}
-      value={title}
-      onChange={e => setTitle(e.target.value)}
-      onBlur={() => setTitle(t => t.trim())}
-      style={{ width: (title ? title.length : placeholder.length) * 11.72 }}
-      autoComplete="off"
-    />
-  );
 
   const isOK = content.replaceAll(XRegExp(`\\P{L}+`, `gu`), "").length >= 16;
 
@@ -60,51 +144,17 @@ export function PostEditor({ user, error: userError }:{ user:Database['public'][
 
   return (
     <div className={styles.post}>
-      <Section
-        title={showPrev ? title : input}
-        className={styles.card}
-        containerClassName={styles.cardContent}
-        isCard
-      >
-        {
-          showPrev ?
-          <MarkDown>
-            {/* TODO: replace the link with an internal `/help/markdown` one */}
-            {content ? content : "### *Input Preview*\n*Try actually doing some [basic markdown](https://www.markdownguide.org/cheat-sheet/#basic-syntax), and some [extended syntax](https://www.markdownguide.org/cheat-sheet/#basic-syntax).*"}
-          </MarkDown>
-          :
-          <textarea
-            name="span"
-            placeholder="What's on your mind? (min. 16)"
-            value={content}
-            onChange={e => { setContent(e.target.value); setError(null); }}
-            onBlur={() => setContent(c => c.trim())}
-            autoComplete="off"
-          />
-        }
-        <Button
-          noMinimum
-          iconBackground
-          className={styles.showPrev}
-          onClick={() => setShowPrev(!showPrev)}
-          color={colors.highlight}
-          icon={{ element: showPrev ? IoEyeOutline : IoEye, }}
-        />
-      </Section>
+      <Editor
+        title={title}
+        content={content}
+        show={showPrev}
+        setTitle={setTitle}
+        setContent={setContent}
+        setShow={setShowPrev}
+        setError={setError as any}
+      />
       <div className={styles.footer}>
-        <div className={styles.asUser}>
-          <span>
-            <C.SECONDARY>
-              Posting as
-            </C.SECONDARY>
-            <C.QUINARY>
-              {' u:'}
-            </C.QUINARY>
-            <C.ACCENT>
-              {user.username}
-            </C.ACCENT>
-          </span>
-        </div>
+        <AsUser username={user.username} />
         <div className={styles.actions}>
           <Button
             noMinimum
@@ -121,41 +171,95 @@ export function PostEditor({ user, error: userError }:{ user:Database['public'][
           />
         </div>
       </div>
-      {
-        error
-        ?
-        <span className={styles.error}>
-          Oops...
-          <br/>
-          {
-            !isOK ?
-            <>
-              <span>
-                That's a really short thought, don't you think?
-              </span>
-            </>
-            :
-            <>
-              <span>
-                Seems an error has occurred, try again?
-              </span>
-              <br/>
-              <span>
-                if that didn't work <a href="https://github.com/IamFastre/the-winery/issues">fill a Github issue</a>.
-                {
-                  Object.keys(error).length ?
-                  <>
-                    <br/>
-                    {JSON.stringify(error)}
-                  </>
-                  : null
-                }
-              </span>
-            </>
-          }
-        </span>
-        : null
+      { error ? <Error error={error} isOK={isOK} /> : null }
+    </div>
+  );
+}
+
+export function DraftEditor({ user, draft }:{ user:Profile; draft:Draft; }) {
+  const router = useRouter();
+  const toaster = useToaster();
+
+  const [title, setTitle] = useState<string>(draft.title ?? "");
+  const [content, setContent] = useState<string>(draft.content);
+  const [showPrev, setShowPrev] = useState<boolean>(true);
+  const [error, setError] = useState<AuthError | PostgrestError | null>();
+
+  const isOK = content.replaceAll(XRegExp(`\\P{L}+`, `gu`), "").length >= 16;
+
+  const onPost = async () => {
+    const { error: postError } = await createPost(title, content);
+    setError(postError as any);
+
+    if (!postError) {
+      const { error: draftError } = await deleteDraft(draft.id);
+      setError(draftError as any);
+
+      if (!draftError) { 
+        toaster.add({ message: "Post added", icon: IoWine });
+        router.push('/profile');
       }
+    }
+  };
+
+  const onEdit = async () => {
+    const { error } = await editDraft(draft.id, title, content);
+    setError(error as any);
+
+    if (!error) {
+      toaster.add({ message: "Changes saved", icon: IoFolder });
+      router.push('/profile/drafts');
+    }
+  };
+
+  const onDelete = async () => {
+    const { error } = await deleteDraft(draft.id);
+    setError(error as any);
+
+    if (!error) {
+      toaster.add({ message: "Draft deleted", icon: RiDeleteBin6Line, type: "error" });
+      router.push('/profile/drafts');
+    }
+  };
+
+  return (
+    <div className={styles.post}>
+      <Editor
+        title={title}
+        content={content}
+        show={showPrev}
+        setTitle={setTitle}
+        setContent={setContent}
+        setShow={setShowPrev}
+        setError={setError as any}
+      />
+      <div className={styles.footer}>
+        <AsUser username={user.username} />
+        <div className={styles.actions}>
+          <Button
+            noMinimum
+            icon={{ element: RiDeleteBin6Line }}
+            onClick={onDelete}
+            color={colors.red}
+            disabled={!user.username}
+            iconBackground
+          />
+          <Button
+            noMinimum
+            icon={{ element: RiBallPenLine }}
+            onClick={onEdit}
+            disabled={!(isOK && user.username)}
+          />
+          <Button
+            noMinimum
+            title="Post"
+            icon={{ element: IoAdd }}
+            onClick={onPost}
+            disabled={!(isOK && user.username)}
+          />
+        </div>
+      </div>
+      { error ? <Error error={error} isOK={isOK} /> : null }
     </div>
   );
 }
--- app/(home)/compose/page.tsx
+++ app/(home)/compose/page.tsx
@@ -1,28 +1,39 @@
 import { Section } from "@/components/Section";
 import { getProfile } from "@/supabase/actions/user";
+import { getDraft } from "@/supabase/actions/post";
+import { Draft } from "@/supabase/actions/types";
 
-import { PostEditor } from "./client";
+import { DraftEditor, PostEditor } from "./client";
 import styles from "./styles.module.scss";
 
 
-export default async function ComposePage() {
+export default async function ComposePage({ searchParams }:{ searchParams: { draft:string | undefined; } }) {
   const { data:user, error } = await getProfile();
+  let draft:Draft | null = null;
 
-  if (!user)
+  if (!user || error)
     return;
+
+  if (searchParams.draft) {  
+    const id = Number.parseInt(searchParams.draft);
+    if (Number.isInteger(id))
+      draft = (await getDraft(id)).data;
+  }
 
   return (
     <Section title="Compose" style={{ flex: 1 }} containerClassName={styles.sectionContent}>
       <div className={styles.title}>
         <span>
-          Create Post
+          {draft ? "Edit Draft" : "Create Post"}
         </span>
         <hr />
       </div>
-      <PostEditor
-        user={user}
-        error={error}
-      />
+      {
+        draft ?
+        <DraftEditor user={user} draft={draft} />
+        :
+        <PostEditor user={user} />
+      }
     </Section>
   );
 }
