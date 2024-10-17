bd157882ef1f9cf0aff061dbb3f630e3a77c2ef6
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-17 19:38:01 +0300

    updating the `api/other/logo` endpoint

--- app/api/other/logo/route.ts
+++ app/api/other/logo/route.ts
@@ -1,17 +1,21 @@
-import { getLogo, LogoKind } from '@/utils';
+import { NextRequest } from 'next/server';
+
+import { getLogo, LogoKind, success } from '@/utils';
 import { getCurrentURL } from '@/utils/server';
-import { NextRequest, NextResponse } from 'next/server';
+
+export type OtherLogo = ArrayBuffer;
+export type OtherLogoParams = { variant:LogoKind | undefined };
 
 export async function GET(request:NextRequest) {
   const { searchParams: params } = new URL(request.url);
 
-  const variant = params.get('variant') as LogoKind | null;
+  const variant = params.get('variant') as OtherLogoParams['variant'];
   const logo = getLogo(variant ?? 'main');
   const url = await getCurrentURL();
   const blob = await (await fetch(`${url}${logo.src}`)).arrayBuffer();
-
+  console.log(`${url}${logo.src}`)
   const headers = new Headers();
   headers.set("Content-Type", "image/png");
 
-  return new NextResponse(blob, { status: 200, statusText: 'OK', headers });
+  return success(blob, headers, false);
 }
