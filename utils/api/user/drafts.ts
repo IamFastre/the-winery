6272e2ca0dd033edad3d4631b64d19c09100028f
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-20 16:43:57 +0300

    changes to saves and drafts endpoints

--- utils/api/user/drafts.ts
+++ utils/api/user/drafts.ts
@@ -2,16 +2,26 @@ import { result } from '@/utils';
 import { createClient } from '@/supabase/server';
 import { Tables } from '@/supabase/types';
 
-export type UserDrafts = Tables<'drafts'>[];
-export type UserDraftsParams = { limit:number };
+export type UserDrafts = { drafts:Tables<'drafts'>[], count:number };
+export type UserDraftsParams = undefined;
 
 export async function getUserDrafts() {
   const supabase = createClient();
 
-  const res = await supabase
+  const draftsRes = await supabase
     .from('drafts')
     .select('*')
     .order('timestamp', { ascending: false });
 
-  return result<UserDrafts | null>(res.data, res.error);
+  if (draftsRes.error)
+    return result(null, draftsRes.error);
+
+  const countRes = await supabase
+    .from('drafts')
+    .select('*', { count: 'exact', head: true });
+
+  if (countRes.error)
+    return result(null, countRes.error);
+
+  return result<UserDrafts>({ drafts: draftsRes.data, count: countRes.count ?? -1 }, draftsRes.error);
 }
--- utils/api/user/saves.ts
+++ utils/api/user/saves.ts
@@ -2,16 +2,26 @@ import { result } from '@/utils';
 import { createClient } from '@/supabase/server';
 import { Tables } from '@/supabase/types';
 
-export type UserSaves = Tables<'saves'>[];
+export type UserSaves = { saves: Tables<'saves'>[], count:number };
 export type UserSavesParams = undefined;
 
 export async function getUserSaves() {
   const supabase = createClient();
 
-  const res = await supabase
+  const savesRes = await supabase
     .from('saves')
     .select('*')
     .order('timestamp', { ascending: false });
 
-  return result<UserSaves | null>(res.data, res.error);
+  if (savesRes.error)
+    return result(null, savesRes.error);
+
+  const countRes = await supabase
+    .from('saves')
+    .select('*', { count: 'exact', head: true });
+
+  if (countRes.error)
+    return result(null, countRes.error);
+
+  return result<UserSaves>({ saves: savesRes.data, count: countRes.count ?? -1 }, savesRes.error);
 }
