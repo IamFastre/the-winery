7f03053bbc0b03b178793a8e67c83fec243e04c0
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-20 16:29:58 +0300

    I kinda forgot `/api/user/posts`

---
+++ app/api/user/posts/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest } from 'next/server';
+
+import { badRequest, notFound, success } from '@/utils';
+import { getUserPosts, UserPosts } from '@/utils/api/user/posts';
+
+export async function GET(request:NextRequest) {
+  const { searchParams: params } = new URL(request.url);
+
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json");
+
+  if (params.has('id')) {
+    const id = params.get('id')!;
+    const res = await getUserPosts('author_uuid', id);
+
+    if (res.error)
+      return notFound(res.error, headers);
+
+    return success<UserPosts>(res.data, headers);
+  }
+
+  if (params.has('username')) {
+    const username = params.get('username')!;
+    const res = await getUserPosts('author', username.toLowerCase());
+
+    if (res.error)
+      return notFound(res.error, headers);
+
+    return success<UserPosts>(res.data, headers);
+  }
+
+  return badRequest("Missing 'id' or 'username' parameter", headers);
+}
---
+++ utils/api/user/posts.ts
@@ -0,0 +1,18 @@
+import { result } from '@/utils';
+import { createClient } from '@/supabase/server';
+import { Tables } from '@/supabase/types';
+
+export type UserPosts = Tables<'posts'>[];
+export type UserPostsParams = { id:string };
+
+export async function getUserPosts(what:'author_uuid' | 'author', value:string) {
+  const supabase = createClient();
+
+  const res = await supabase
+    .from('posts')
+    .select('*')
+    .eq(what, value)
+    .order('timestamp', { ascending: false });
+
+  return result<UserPosts | null>(res.data, res.error);
+}
