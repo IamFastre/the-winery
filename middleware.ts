8d1a6a6e3e07149b1733d3fcc28a0c7afc97ef20
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-27 22:18:18 +0300

    middleware now sets a request url header under `x-request-url` name

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/supabase/middleware';
 
 export async function middleware(request:NextRequest) {
+  request.headers.set('x-request-url', request.url);
   return await updateSession(request);
 }
 
