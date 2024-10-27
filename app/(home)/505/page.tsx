f8d56d2847b7037989b5e497743423c737bcf86d
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-10-28 01:47:04 +0300

    added the worst dad joke there is probably

---
+++ app/(home)/505/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { IoBug, IoClose } from "react-icons/io5";
+
+import { B, C } from "@/components/C";
+import { Section } from "@/components/Section";
+
+import styles from "./styles.module.scss";
+
+export default function E505() {
+  return (
+    <Section
+      style={{ flex: 1 }}
+      containerClassName={styles.sectionContent}
+    >
+      <div className={styles.box}>
+        <div className={styles.text}>
+          <Link className={styles.closeButton} href="/" type="wrapper">
+            <IoClose />
+          </Link>
+          <h2>
+            <IoBug />
+            Uh-oh?
+          </h2>
+          <C.RED>
+            Error 505: <C.SECONDARY><B>Song found instead!</B></C.SECONDARY>
+          </C.RED>
+        </div>
+        <div
+          className={styles.iframeHolder}
+          dangerouslySetInnerHTML={{ __html: `
+            <iframe
+              width="500"
+              height="232"
+              style="border-radius:12px"
+              src="https://open.spotify.com/embed/track/0BxE4FqsDD1Ot4YuBXwAPp?utm_source=generator&theme=0"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            />
+          `}}
+        />
+      </div>
+    </Section>
+  );
+}
