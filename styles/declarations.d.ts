9832acff79489ab64c53995cbf8390d46f7fa55c
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-09 03:00:11 +0200

    colors ts declaration

---
+++ styles/declarations.d.ts
@@ -0,0 +1,27 @@
+declare module '@/styles/colors.module.scss' {
+  const none: string;
+
+  const primary: string;
+  const secondary: string;
+  const tertiary: string;
+  const quaternary: string;
+  const quinary: string;
+
+  const accent: string;
+  const highlight: string;
+
+  const hot: string;
+  const cold: string;
+
+  const gold: string;
+  const wine: string;
+  const orange: string;
+
+  const red: string;
+  const green: string;
+  const blue: string;
+
+  const cyan: string;
+  const yellow: string;
+  const magenta: string;
+}
