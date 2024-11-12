b8138d214a334dfbbdad121f9bd715da5d349b6d
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-12 15:06:00 +0200

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,42 +19,52 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${currentURL}`,
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${currentURL}/getting-started`,
+      changeFrequency: "monthly",
       priority: 0.9,
     },
     {
       url: `${currentURL}/auth/signup`,
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${currentURL}/auth/login`,
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${currentURL}/search`,
+      changeFrequency: "monthly",
       priority: 0.75,
     },
     {
       url: `${currentURL}/compose`,
+      changeFrequency: "monthly",
       priority: 0.7,
     },
     {
       url: `${currentURL}/auth/password/forgot`,
+      changeFrequency: "monthly",
       priority: 0.7,
     },
     {
       url: `${currentURL}/drafts`,
+      changeFrequency: "monthly",
       priority: 0.6,
     },
     {
       url: `${currentURL}/saved`,
+      changeFrequency: "monthly",
       priority: 0.6,
     },
     ...userSlugs.map(slug => ({
       url: `${currentURL}/u/${slug}`,
+      changeFrequency: "daily" as const,
       priority: 0.7,
     })),
   ];
