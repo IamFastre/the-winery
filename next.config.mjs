e9901e6c33769cf52e64d18c92e88d41dab2dc65
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-13 14:56:51 +0200

    new way to import SVGs

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack(config) {
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
+    return config;
+  },
 };
 
 export default nextConfig;
