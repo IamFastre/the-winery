66243b29b514e5b33d8395d2485ac7c599bee579
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-13 06:21:49 +0200

    getting ready for zodiacs

--- utils/funcs.ts
+++ utils/funcs.ts
@@ -10,6 +10,34 @@ import NDCBO from "@/public/static/images/logo/NaipeDeCopasBrandO.png";
 import NDCMO from "@/public/static/images/logo/NaipeDeCopasMonoO.png";
 import NDCTI from "@/public/static/images/logo/NaipeDeCopasTouch.png";
 
+import ZodiacAquarius    from "@/public/static/images/zodiacs/Aquarius.svg";
+import ZodiacPisces      from "@/public/static/images/zodiacs/Pisces.svg";
+import ZodiacAries       from "@/public/static/images/zodiacs/Aries.svg";
+import ZodiacTaurus      from "@/public/static/images/zodiacs/Taurus.svg";
+import ZodiacGemini      from "@/public/static/images/zodiacs/Gemini.svg";
+import ZodiacCancer      from "@/public/static/images/zodiacs/Cancer.svg";
+import ZodiacLeo         from "@/public/static/images/zodiacs/Leo.svg";
+import ZodiacVirgo       from "@/public/static/images/zodiacs/Virgo.svg";
+import ZodiacLibra       from "@/public/static/images/zodiacs/Libra.svg";
+import ZodiacScorpio     from "@/public/static/images/zodiacs/Scorpio.svg";
+import ZodiacSagittarius from "@/public/static/images/zodiacs/Sagittarius.svg";
+import ZodiacCapricorn   from "@/public/static/images/zodiacs/Capricorn.svg";
+
+
+export const ZODIAC_SVG = {
+  aquarius:    ZodiacAquarius,
+  pisces:      ZodiacPisces,
+  aries:       ZodiacAries,
+  taurus:      ZodiacTaurus,
+  gemini:      ZodiacGemini,
+  cancer:      ZodiacCancer,
+  leo:         ZodiacLeo,
+  virgo:       ZodiacVirgo,
+  libra:       ZodiacLibra,
+  scorpio:     ZodiacScorpio,
+  sagittarius: ZodiacSagittarius,
+  capricorn:   ZodiacCapricorn,
+}
 
 export function multiplyString(str:string, num:number) : string {
   let res = "";
@@ -53,12 +81,52 @@ export function getLogo(kind:LogoKind) : StaticImageData {
        : NDC;
 }
 
+export function getZodiac(day:number, month:number) {
+  if ((month === 1 && day >= 20) || (month === 2 && day <= 18))
+    return ZodiacAquarius;
+
+  if ((month === 2 && day >= 19) || (month === 3 && day <= 20))
+    return ZodiacPisces;
+
+  if ((month === 3 && day >= 21) || (month === 4 && day <= 19))
+    return ZodiacAries;
+
+  if ((month === 4 && day >= 20) || (month === 5 && day <= 20))
+    return ZodiacTaurus;
+
+  if ((month === 5 && day >= 21) || (month === 6 && day <= 20))
+    return ZodiacGemini;
+
+  if ((month === 6 && day >= 21) || (month === 7 && day <= 22))
+    return ZodiacCancer;
+
+  if ((month === 7 && day >= 23) || (month === 8 && day <= 22))
+    return ZodiacLeo;
+
+  if ((month === 8 && day >= 23) || (month === 9 && day <= 22))
+    return ZodiacVirgo;
+
+  if ((month === 9 && day >= 23) || (month === 10 && day <= 22))
+    return ZodiacLibra;
+
+  if ((month === 10 && day >= 23) || (month === 11 && day <= 21))
+    return ZodiacScorpio;
+
+  if ((month === 11 && day >= 22) || (month === 12 && day <= 21))
+    return ZodiacSagittarius;
+
+  if ((month === 12 && day >= 22) || (month === 1 && day <= 19))
+    return ZodiacCapricorn;
+
+  return null;
+}
+
 export function humanizeTime(stamp: number | string, utc:boolean = true, noTime:boolean = false) : string {
   const now  = utc ? moment.utc() : moment();
   const date = utc ? moment.utc(stamp) : moment(stamp);
 
-  let dif = now.date() - date.date();
-  let mif = now.month() - date.month();
+  const dif = now.date() - date.date();
+  const mif = now.month() - date.month();
 
   let day = now.year() === date.year() && now.month() === date.month()
           ? (dif === 0 ? "today" : dif === 1 ? "yesterday" : `${dif} days ago`)
