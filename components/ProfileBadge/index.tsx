41b977cba55ca77fe57c0e73b617d031ac5998fe
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-21 01:21:49 +0200

    profile badge component

---
+++ components/ProfileBadge/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+
+import { focusable } from "@/utils/client";
+import { MenuModal } from "@/components";
+import { Modal } from "@/providers/ModalProvider";
+
+import styles from "./style.module.scss";
+
+interface ProfileBadgeProps {
+  children: React.ReactNode;
+  condition?: boolean;
+  title: string;
+  description: React.ReactNode;
+}
+
+export function ProfileBadge(props:ProfileBadgeProps) {
+  const modalShown = useState<boolean>(false);
+
+  if (props.condition === false)
+    return null;
+
+  return (
+    <>
+      <div className={styles.badge} {...focusable(styles.active, () => modalShown[1](s => !s))}>
+        {props.children}
+      </div>
+      
+      <Modal state={modalShown}>
+        <MenuModal title={props.title} close={() => modalShown[1](false)}>
+          {props.description}
+        </MenuModal>
+      </Modal>
+    </>
+  );
+}
