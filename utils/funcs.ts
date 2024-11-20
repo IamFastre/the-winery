e87d0193c1d9cf141a56c203eb293a20997f2e24
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-11-21 01:19:25 +0200

    more util funcs

--- utils/funcs.ts
+++ utils/funcs.ts
@@ -33,6 +33,10 @@ export function multiplyString(str:string, num:number) : string {
   return res;
 }
 
+export function vowelStart(str:string) : boolean {
+  return /^[aeiou]/i.test(str);
+}
+
 export function hexOpacity(str:string, opacity:number) : string {
   if (str[0] !== '#')
     throw new Error("Must be a hex color starting with '#'");
@@ -69,40 +73,56 @@ export function getLogo(kind:LogoKind) : StaticImageData {
 export function getZodiac(day:number, month:number) : React.FC<React.SVGProps<SVGSVGElement>> | null {
   if ((month === 1 && day >= 20) || (month === 2 && day <= 18))
     return ZodiacAquarius;
-
   if ((month === 2 && day >= 19) || (month === 3 && day <= 20))
     return ZodiacPisces;
-
   if ((month === 3 && day >= 21) || (month === 4 && day <= 19))
     return ZodiacAries;
-
   if ((month === 4 && day >= 20) || (month === 5 && day <= 20))
     return ZodiacTaurus;
-
   if ((month === 5 && day >= 21) || (month === 6 && day <= 20))
     return ZodiacGemini;
-
   if ((month === 6 && day >= 21) || (month === 7 && day <= 22))
     return ZodiacCancer;
-
   if ((month === 7 && day >= 23) || (month === 8 && day <= 22))
     return ZodiacLeo;
-
   if ((month === 8 && day >= 23) || (month === 9 && day <= 22))
     return ZodiacVirgo;
-
   if ((month === 9 && day >= 23) || (month === 10 && day <= 22))
     return ZodiacLibra;
-
   if ((month === 10 && day >= 23) || (month === 11 && day <= 21))
     return ZodiacScorpio;
-
   if ((month === 11 && day >= 22) || (month === 12 && day <= 21))
     return ZodiacSagittarius;
-
   if ((month === 12 && day >= 22) || (month === 1 && day <= 19))
     return ZodiacCapricorn;
+  return null;
+}
 
+export function getZodiacString(day:number, month:number) : string | null {
+  if ((month === 1 && day >= 20) || (month === 2 && day <= 18))
+    return 'Aquarius';
+  if ((month === 2 && day >= 19) || (month === 3 && day <= 20))
+    return 'Pisces';
+  if ((month === 3 && day >= 21) || (month === 4 && day <= 19))
+    return 'Aries';
+  if ((month === 4 && day >= 20) || (month === 5 && day <= 20))
+    return 'Taurus';
+  if ((month === 5 && day >= 21) || (month === 6 && day <= 20))
+    return 'Gemini';
+  if ((month === 6 && day >= 21) || (month === 7 && day <= 22))
+    return 'Cancer';
+  if ((month === 7 && day >= 23) || (month === 8 && day <= 22))
+    return 'Leo';
+  if ((month === 8 && day >= 23) || (month === 9 && day <= 22))
+    return 'Virgo';
+  if ((month === 9 && day >= 23) || (month === 10 && day <= 22))
+    return 'Libra';
+  if ((month === 10 && day >= 23) || (month === 11 && day <= 21))
+    return 'Scorpio';
+  if ((month === 11 && day >= 22) || (month === 12 && day <= 21))
+    return 'Sagittarius';
+  if ((month === 12 && day >= 22) || (month === 1 && day <= 19))
+    return 'Capricorn';
   return null;
 }
 
