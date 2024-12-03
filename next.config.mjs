df9aed7000c64f523a0e3c6b0628f7a1d95e8133
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-12-03 04:03:08 +0200

    using turbo for loading SVGs

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,15 @@ const nextConfig = {
     implementation: 'sass-embedded',
     silenceDeprecations: ['legacy-js-api'],
   },
-  webpack(config) {
-    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
-    return config;
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
 };
 
