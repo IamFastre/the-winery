0bad3781a7758619809ef0feaf206295089d39d2
IamFastre <61952761+IamFastre@users.noreply.github.com>
2024-12-12 18:31:45 +0200

    apparently TurboPack isn't the one used for production builds so we'll have to configure WebPack as well... fuck this shit, man

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,11 @@ const nextConfig = {
       },
     },
   },
+
+  webpack(config) {
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
+    return config;
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
