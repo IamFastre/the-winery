1339937646af543df9bf7e23568b33660d96c414
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-01-08 07:07:50 +0200

    fun little thing until I actually add the setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata, Viewport } from "next";
 
 import consts from "@/utils/consts";
+import { getUserInfo } from "@/utils/api/user/info";
 import { Shortcuts } from "@/providers/Shortcuts";
 import { QueryProvider } from "@/providers/Query";
 import { Toaster } from "@/providers/Toaster";
 import { ModalProvider } from "@/providers/ModalProvider";
 
-import { MakeTheme, DARK } from "@/styles/themes";
+import { MakeTheme, DARK, SCARLATTA, LIGHT } from "@/styles/themes";
 import "@/styles/globals.scss";
 
 export const metadata:Metadata = {
@@ -21,9 +22,11 @@ export const viewport:Viewport = {
   width: 'device-width',
 }
 
-export default function RootLayout({ children }: Readonly<{ children:React.ReactNode }>) {
+export default async function RootLayout({ children }: Readonly<{ children:React.ReactNode }>) {
+  const { data } = await getUserInfo('self');
+
   return (
-    <html lang="en" style={MakeTheme(DARK)}>
+    <html lang="en" style={MakeTheme(data?.gender === 'female' ? SCARLATTA : data?.gender === 'toaster' ? LIGHT : DARK)}>
       <head>
         <meta name="google-site-verification" content="c2u4sSYzf3Z23dbDg1eiq_nJzXDm7aLScvANNt59pvI" />
       </head>
