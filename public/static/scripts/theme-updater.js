6d8e7e9fa9ac1728496339782cbdb1bcfb9a4679
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-01-15 07:06:08 +0200

    changing the `data-theme` attribution now changes the local storage entry

--- public/static/scripts/theme-updater.js
+++ public/static/scripts/theme-updater.js
@@ -1,10 +1,33 @@
+const HTML_THEME_ATTR_NAME = "data-theme";
+const THEME_DEF_ATTR_NAME  = "data-theme-definition";
+const STORAGE_THEME_ENTRY  = "settings:theme";
+
+const html = document.children[0];
+
+const observer = new MutationObserver(mutationsList => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'attributes' && mutation.attributeName === HTML_THEME_ATTR_NAME) {
+      const theme = html.getAttribute(HTML_THEME_ATTR_NAME);
+      const allThemes = GetAvailableThemes();
+
+      if (allThemes.includes(theme)) {
+        SetTheme(theme);
+      } else {
+        html.setAttribute(HTML_THEME_ATTR_NAME, allThemes[0]);
+        SetTheme(allThemes[0]);
+      }
+    }
+  }
+});
+
+
 function GetAvailableThemes() {
   const r = [];
   for (let i = 0; i < document.styleSheets.length; i++) {
     const ss = document.styleSheets[i];
     const owner = ss.ownerNode;
-    if (owner.hasAttribute("data-theme-definition")) {
-      r.push(owner.getAttribute("data-theme-definition"));
+    if (owner.hasAttribute(THEME_DEF_ATTR_NAME)) {
+      r.push(owner.getAttribute(THEME_DEF_ATTR_NAME));
     }
   }
 
@@ -12,17 +35,25 @@ function GetAvailableThemes() {
 }
 
 function UpdateTheme(t = undefined) {
-  const all_themes = GetAvailableThemes();
-  const theme = t ?? LocalStorage.get("settings:theme");
-  document.children[0].setAttribute(
-    "data-theme",
-    all_themes.includes(theme) ? theme : all_themes[0]
+  const allThemes = GetAvailableThemes();
+  const theme = t ?? LocalStorage.get(STORAGE_THEME_ENTRY);
+  html.setAttribute(
+    HTML_THEME_ATTR_NAME,
+    allThemes.includes(theme) ? theme : allThemes[0]
   );
 }
 
+function SetTheme(t) {
+  const allThemes = GetAvailableThemes();
+  if (allThemes.includes(t)) {
+    LocalStorage.set(STORAGE_THEME_ENTRY, t);
+  }
+}
+
 UpdateTheme();
+observer.observe(html, { attributes: true });
 
 addEventListener('storage', e => {
-  if (e.key === "settings:theme")
+  if (e.key === STORAGE_THEME_ENTRY)
     UpdateTheme();
 });
