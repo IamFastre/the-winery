f37ff991e616ba5b92fa5547f52edf1097cf501a
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-01-20 07:05:09 +0200

    settings page client side stuff, more to come

---
+++ app/(home)/settings/client.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useState } from "react";
+import { IoColorPalette } from "@icons/io5/IoColorPalette";
+import { IoBrushOutline } from "@icons/io5/IoBrushOutline";
+
+import { options } from "@/utils/consts";
+import { DropdownButton } from "@/components/DropdownButton";
+
+import styles from "./styles.module.scss";
+import { IconType } from "@react-icons/all-files";
+
+const Header = ({ title, Icon }:{ title:string; Icon?:IconType }) => (
+  <span id={title.toLowerCase().replaceAll(/\W+/g, "-")} className={styles.header}>
+    {title}
+    {Icon && <Icon />}
+  </span>
+);
+
+const Setting = ({ title, description, children }:{ title:string; description?:string; children:React.ReactNode; }) => {
+  return (
+    <div className={styles.setting}>
+      <div className={styles.title}>
+        <span>
+          {title}
+        </span>
+      </div>
+      { description &&
+        <div className={styles.description}>
+          {description}
+        </div>
+      }
+      {children}
+    </div>
+  );
+};
+
+export function ThemeSetting() {
+  const [themeI, setThemeI] = useState<number>(
+    options['settings']['theme'].indexOf(
+      document.children[0].getAttribute("data-theme") ?? 'dark'
+    )
+  );
+
+  const onSelectTheme = (o:string, i:number) => {
+    document.children[0].setAttribute("data-theme", o.toLowerCase());
+    setThemeI(i);
+  };
+
+  return (
+    <Setting
+      title="Theme"
+      description="decides the color palette and styling for your client."
+    >
+      <DropdownButton
+        title={options['settings']['theme'][themeI]}
+        icon={IoColorPalette}
+        onSelect={onSelectTheme}
+        selectedIndices={[themeI]}
+        options={options['settings']['theme']}
+        style="bottom"
+        noWith
+      />
+    </Setting>
+  );
+}
+
+export function Settings() {
+  return (
+    <div className={styles.settings}>
+      <Header title="Appearance" Icon={IoBrushOutline} />
+      <ThemeSetting />
+    </div>
+  );
+}
