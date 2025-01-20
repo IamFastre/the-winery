df4262b4c6ff59d600e8f0299bafbab72fef9f6f
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-01-20 06:20:05 +0200

    new consts

--- utils/consts.ts
+++ utils/consts.ts
@@ -5,11 +5,19 @@ const consts = {
   name: "The Winery",
   shortname: "Winery",
   userlabel: "Winer",
+  organization: "neRIA",
 } as const;
 
 export const options = {
   feed: {
     "sort-by": ['default', 'new', 'random'] as const
+  },
+  settings: {
+    "theme": [
+      "dark",
+      "light",
+      "scarlatta",
+    ]
   }
 };
 
