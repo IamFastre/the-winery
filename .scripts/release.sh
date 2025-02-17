007c0a1500de39da53244cce57916652108bb669
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-02-17 19:08:02 +0200

    updated the release script to be hawt

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 source .scripts/init.sh
 
-npm version $1 --no-git-tag-version
-git add .
-git commit -m "VERSION $1"
+v=$1
+do_push=false
 
-if [ "$2" == "-p" ]; then
-    git push
+printf '•> Releasing version: %s' "$v"
+printf '\n'
+
+for p in "$@"; do
+    if [ $p == '-p' ] && [ $do_push == false ]; then
+        printf '•> Script will push directly'
+        printf '\n'
+        do_push=true
+    fi
+done
+
+try
+( npm version $v --no-git-tag-version > /dev/null 2>&1 || throw )
+
+catch || end $EXIT_CODE "Provided version is invalid"
+
+git add .
+git commit -m "VERSION $v" > /dev/null
+
+printf '•> Commit made'
+printf '\n'
+
+if [ $do_push == true ]; then
+    try
+    ( git push )
+    catch  || end $EXIT_CODE "Could not push commit"
 fi
 
 end
