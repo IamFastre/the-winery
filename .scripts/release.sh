c5f6110b81004562c1bb8fae513188203fc8dc67
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-02-17 19:36:00 +0200

    updated the release script to be even hawter

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -2,7 +2,18 @@
 source .scripts/init.sh
 
 v=$1
+m="VERSION $v"
+release_type=
+has_type=false
 do_push=false
+
+check_type() {
+    if [ -z "$release_type" ]; then
+        release_type=$1
+    else
+        end 1 "Release type already set to '$release_type' trying to set it to '$1'"
+    fi
+}
 
 printf '•> Releasing version: %s' "$v"
 printf '\n'
@@ -12,16 +23,34 @@ for p in "$@"; do
         printf '•> Script will push directly'
         printf '\n'
         do_push=true
+
+    elif [ $p == '-r' ]; then
+        check_type $p
+        printf '•> Version set to be a re-release'
+        printf '\n'
+        m="reVERSION $v"
+
+    elif [ $p == '-b' ]; then
+        check_type $p
+        printf '•> Version set to be a beta'
+        printf '\n'
+        m="BETA $v"
+
+    elif [ $p == '-f' ]; then
+        check_type $p
+        printf '•> Version set to be a hot fix'
+        printf '\n'
+        m="HOTFIX $v"
     fi
 done
 
 try
-( npm version $v --no-git-tag-version > /dev/null 2>&1 || throw )
+( npm version "$v" --no-git-tag-version > /dev/null 2>&1 || throw )
 
 catch || end $EXIT_CODE "Provided version is invalid"
 
 git add .
-git commit -m "VERSION $v" > /dev/null
+git commit -m "$m" > /dev/null
 
 printf '•> Commit made'
 printf '\n'
